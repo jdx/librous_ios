f591c1c6d6cd02b3ff9beedc153dfcdb1fc064d7
dickeyxxx <jeff@dickeyxxx.com>
2014-01-11 20:58:12 -0800

    Initial Commit

---
+++ librous/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  librous
+//
+//  Created by Jeff Dickey on 1/11/14.
+//  Copyright (c) 2014 dickeyxxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
